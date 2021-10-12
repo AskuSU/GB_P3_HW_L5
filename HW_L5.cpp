f018110eda1ae9b774d75f1a1efee74bb2f76f2a
Asku_SU <zaharandrei15@mail.ru>
2021-10-12 16:37:07 +0300

    Сделал 1ю

--- HW_L5.cpp
+++ HW_L5.cpp
@@ -1,15 +1,27 @@
 ﻿#include <windows.h>
 
 #include "MyLib.h"
-
+#include "ContainerExt.h"
 
 
 void Task1()
 {
-	std::cout << "Работа с Функцией insert_sorted:" << std::endl << std::endl;
-	
-	
+	std::cout << "Работа с Функцией printUniqueElements:" << std::endl << std::endl;
 
+	std::vector<std::string> strings{ "one", "three", "two", "four", "six", "five", "seven", "one", "three", "six" };
+	std::cout << "Исходный контейнер Vector:" << std::endl;
+	myLib::printContainer(strings);
+
+	std::cout << std::endl << "Результат:" << std::endl;
+	printUniqueElements<std::string>(strings.begin(), strings.end());
+	std::cout << std::endl;
+
+	std::list<std::string> stringsL{ "one", "three", "two", "four", "six", "five", "seven", "eleven", "three", "ten" };
+	std::cout << "Исходный контейнер List:" << std::endl;
+	myLib::printContainer(stringsL);
+
+	std::cout << std::endl << "Результат:" << std::endl;
+	printUniqueElements<std::string>(stringsL.begin(), stringsL.end());
 	std::cout << std::endl;
 }
 
